cdba4492e1d1ad2c44c58367ad0e4f1eedf3d4da
Kyrie Elieson A. Garcera <kagarcera@trends.com.ph>
2022-03-22 22:31:45 +0800

    update

--- app/controllers/message_controller.ts
+++ app/controllers/message_controller.ts
@@ -5,18 +5,18 @@ exports.message = async(req: Request, res: Response)=>{
     let check_jsonFormat = JSON.parse(JSON.stringify(req.body));
     try {
         if (check_jsonFormat) {
-        let check_format = Object.keys(req.body);
-        const validate_id = check_format.includes('conversation_id');
-        const validate_message = check_format.includes('message');
-        if (validate_id === false || validate_message === false) {
-            return res.send({ message: 'Invalid json format' }).status(400);
-        } else {
-            const conversation_id: any = req.body.conversation_id; 
-            const message: string = req.body.message.split(' ')[0];
-            const return_val = validate_string(message);
-            const data = { response_id: conversation_id, response: return_val};     
-            res.send(data).status(200)
-        }
+            let check_format = Object.keys(req.body);
+            const validate_id = check_format.includes('conversation_id');
+            const validate_message = check_format.includes('message');
+            if (validate_id === false || validate_message === false) {
+                return res.send({ message: 'Invalid json format' }).status(400);
+            } else {
+                const conversation_id: any = req.body.conversation_id; 
+                const message: string = req.body.message.split(' ')[0];
+                const return_val = validate_string(message);
+                const data = { response_id: conversation_id, response: return_val};     
+                res.send(data).status(200)
+            }
         }
 
     } catch (err) {
